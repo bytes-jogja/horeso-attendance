044f9b22621ec64a1f3a22bf65754ff59d81fe7d
Alie Pratama <102345023+aliepratama@users.noreply.github.com>
2023-07-11 13:15:34 +0700

    Inisialisasi projek modul

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'HoReSo Attendances',
+    'version': '1.0',
+    'category': 'Human Resources/Attendances',
+    'summary': 'HoReSo attendance',
+    'description': 'Horeso Attendance',
+    'depends': ['hr', 'barcodes'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
